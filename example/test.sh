338eb8bfa72c31dbbd1952d4a49ae583a2887281
Vorapol Anjalisangas <thaithonpe@gmail.com>
2011-05-06 20:42:52 +0700

    fixed, and change few directory structures

--- example/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for i in `ls -R` ; do
-	if [ -f $i ] ; then
-		python ../thaipe.py $PWD/$i
-	fi
-done
--- jslib/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-The makeJsLib.py file is going to be called in thaipe.py, thus this file is
-required to make this folder importable without putting it in sys.path
-"""
